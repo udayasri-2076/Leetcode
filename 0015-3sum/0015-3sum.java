fc53b625566a6471140534d6096e5cf54cec06f6
Udayasri <udayas1220@gmail.com>
2026-09-12 19:38:58 +0530

    Time: 33 ms (70.77%), Space: 59.4 MB (32.1%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,47 +1,48 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-
-        List<List<Integer>> res=new ArrayList<>();
         
         int n=nums.length;
+
         Arrays.sort(nums);
 
-        for(int i=0;i<n-2;i++){
-            if(i>0 && nums[i]==nums[i-1]) continue;
+        List<List<Integer>> result= new ArrayList<>();
 
-            int l=i+1;
-            int r=n-1;
+        for(int i=0;i<n;i++){
+            if(i>0 && nums[i]==nums[i-1]){
+                continue;
+            }
 
-            while(l<r){
-                int sum=nums[i]+nums[l]+nums[r];
+            int j=i+1;
+            int k=n-1;
+
+            while(j<k){
+                int sum=nums[i]+nums[j]+nums[k];
 
                 if(sum==0){
-                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    result.add(Arrays.asList(nums[i],nums[j],nums[k]));
+                    j++;
+                    k--;
 
+                    while(j<k && nums[j]==nums[j-1]){
+                        j++;
+                    }
 
-                    while(l<r && nums[l]==nums[l+1]) l++;
-
-                    while(l<r && nums[r]==nums[r-1]) r--;
-
-
-                    l++;
-                    r--;
-
-                     
-
+                    while(j<k && nums[k]==nums[k+1]){
+                        k--;
+                    }
                 }
 
-                else if(sum>0){
-                    r--;
+                else if(sum<0){
+                    j++;
                 }
 
                 else{
-                    l++;
+                    k--;
                 }
             }
-
         }
 
-        return res;
+        return result;
+
     }
 }
